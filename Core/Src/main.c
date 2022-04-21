4454e987c6f0e38650e38b4b5266a55d0ebfd0a4
Daniel Francois Moller <21593698@sun.ac.za>
2022-04-21 22:40:46 +0200

    Demo 3 - LCD Hello World Implemented

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -36,6 +36,25 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+
+// LCD instructions
+#define lcd_instruction_ClearDisplay    	0b00000001      // replace all characters with ASCII 'space'
+#define lcd_instruction_EntryMode       	0b00000110      // shift cursor from left to right on read/write
+#define lcd_instruction_DisplayOn      		0b00001111      // Display On, Cursor On, Cursor Blinking On (0b00001DCB)
+#define lcd_instruction_FunctionReset   	0b00110000      // reset the LCD
+#define lcd_instruction_FunctionSet4bit 	0b00101000      // 4-bit data, 2-line display, 5 x 7 font
+#define lcd_instruction_DisplayShiftRight	0b00011100
+#define lcd_instruction_DisplayShiftLeft	0b00011000
+#define lcd_instruction_CursorShiftRight	0b00010100
+#define lcd_instruction_CursorShiftLeft		0b00010000
+
+#define lcd_RS_bit			13
+#define lcd_E_bit			15
+#define lcd_D4_bit			6
+#define lcd_D5_bit			8
+#define lcd_D6_bit			11
+#define lcd_D7_bit			12
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -67,6 +86,14 @@ void interpret_rx_message(uint8_t *rx_array, uint8_t length);
 void request_measurement(uint8_t parameter);
 void request_status();
 void set_output_parameter(uint8_t *rx_array, uint8_t length);
+void LCD_Init();
+void LCD_Write_Character(uint8_t character);
+void LCD_Write_Instruction(uint8_t instruction);
+void LCD_Write_Nibbles(uint8_t byte);
+void LCD_Write_8bitInstruction(uint8_t byte);
+void LCD_ZeroPins();
+void LCD_Write_String(uint8_t string[]);
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -180,6 +207,18 @@ int main(void)
   // Set up the default state of the device
   display_state = changeDisplayState(1); // 1 == Measurement mode
 
+  // Test register writing
+//  lcd_RS_GPIO_Port->ODR |= (1<<lcd_RS_bit);
+  LCD_Init();
+  uint8_t screen_message[] = "Hello World!";
+  LCD_Write_String(screen_message);
+
+//  lcd_RS_GPIO_Port->ODR |= (1<<lcd_RS_bit);
+//  lcd_E_GPIO_Port->ODR |= (1<<lcd_E_bit);
+//  lcd_D4_GPIO_Port->ODR |= (1<<lcd_D4_bit);
+//  lcd_D5_GPIO_Port->ODR |= (1<<lcd_D5_bit);
+//  lcd_D6_GPIO_Port->ODR |= (1<<lcd_D6_bit);
+//  lcd_D7_GPIO_Port->ODR |= (1<<lcd_D7_bit);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -526,10 +565,14 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LD2_Pin|LD5_Pin|LD4_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LD2_Pin|LD5_Pin|LD4_Pin|lcd_D6_Pin 
+                          |lcd_D7_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4|LD3_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4|lcd_D4_Pin|LD3_Pin|lcd_D5_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, lcd_blown_Pin|lcd_RS_Pin|lcd_E_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -568,6 +611,34 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : lcd_blown_Pin */
+  GPIO_InitStruct.Pin = lcd_blown_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(lcd_blown_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : lcd_RS_Pin lcd_E_Pin */
+  GPIO_InitStruct.Pin = lcd_RS_Pin|lcd_E_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : lcd_D4_Pin lcd_D5_Pin */
+  GPIO_InitStruct.Pin = lcd_D4_Pin|lcd_D5_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : lcd_D6_Pin lcd_D7_Pin */
+  GPIO_InitStruct.Pin = lcd_D6_Pin|lcd_D7_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
@@ -848,6 +919,118 @@ void set_output_parameter(uint8_t *rx_array, uint8_t length)
 			// Problems
 			break;
 	}
+}
+
+// LCD Functions #################
+
+void LCD_Init()
+{
+
+	HAL_UART_Transmit(&huart2, (uint8_t*)"\nStart of Init Function:\n", 25, 10);
+
+	HAL_Delay(20);
+
+	LCD_Write_8bitInstruction(lcd_instruction_FunctionReset);
+	HAL_Delay(10);
+
+	LCD_Write_8bitInstruction(lcd_instruction_FunctionReset);
+	HAL_Delay(1);
+
+	LCD_Write_8bitInstruction(lcd_instruction_FunctionReset);
+	HAL_Delay(10);
+
+	LCD_Write_8bitInstruction(lcd_instruction_FunctionSet4bit);
+	HAL_Delay(1);
+
+	LCD_Write_Instruction(lcd_instruction_FunctionSet4bit);
+	HAL_Delay(1);
+
+	LCD_Write_Instruction(lcd_instruction_DisplayOn);
+	HAL_Delay(1);
+
+	LCD_Write_Instruction(lcd_instruction_ClearDisplay);
+	HAL_Delay(3);
+
+	LCD_Write_Instruction(lcd_instruction_EntryMode);
+	HAL_Delay(1);
+
+	HAL_UART_Transmit(&huart2, (uint8_t*)"\nEnd of Init Function:\n", 23, 10);
+
+}
+
+void LCD_Write_String(uint8_t string[])
+{
+	int i = 0;
+	while (string[i] != 0)
+	{
+		LCD_Write_Character(string[i]);
+		i++;
+		HAL_Delay(1);
+	}
+}
+
+void LCD_Write_Character(uint8_t character)
+{
+	lcd_RS_GPIO_Port->ODR |= (1<<lcd_RS_bit);			// select data register (RS High)
+	LCD_Write_Nibbles(character);
+}
+
+void LCD_Write_Instruction(uint8_t instruction)
+{
+	lcd_RS_GPIO_Port->ODR &= ~(1<<lcd_RS_bit);			// select the Instruction Register (RS low)
+	LCD_Write_Nibbles(instruction);
+}
+
+void LCD_Write_Nibbles(uint8_t byte)
+{
+	lcd_E_GPIO_Port->ODR |= (1<<lcd_E_bit);			// set E high
+	LCD_ZeroPins();
+
+	// Write first (most significant) nibble
+	if(byte & 1<<7)	lcd_D7_GPIO_Port->ODR |= (1<<lcd_D7_bit);
+	if(byte & 1<<6)	lcd_D6_GPIO_Port->ODR |= (1<<lcd_D6_bit);
+	if(byte & 1<<5)	lcd_D5_GPIO_Port->ODR |= (1<<lcd_D5_bit);
+	if(byte & 1<<4)	lcd_D4_GPIO_Port->ODR |= (1<<lcd_D4_bit);
+
+
+	// Pulse Enable
+	HAL_Delay(1);
+	lcd_E_GPIO_Port->ODR &= ~(1<<lcd_E_bit);			// Set to 0
+	lcd_E_GPIO_Port->ODR |= (1<<lcd_E_bit);				// Set to 1
+
+	LCD_ZeroPins();
+	if(byte & 1<<3)	lcd_D7_GPIO_Port->ODR |= (1<<lcd_D7_bit);
+	if(byte & 1<<2)	lcd_D6_GPIO_Port->ODR |= (1<<lcd_D6_bit);
+	if(byte & 1<<1)	lcd_D5_GPIO_Port->ODR |= (1<<lcd_D5_bit);
+	if(byte & 1<<0)	lcd_D4_GPIO_Port->ODR |= (1<<lcd_D4_bit);
+
+	// Drop Enable
+	HAL_Delay(1);
+	lcd_E_GPIO_Port->ODR &= ~(1<<lcd_E_bit);		// Set to 0
+}
+
+void LCD_Write_8bitInstruction(uint8_t byte)
+{
+	lcd_RS_GPIO_Port->ODR &= ~(1<<lcd_RS_bit);			// Set RS to 0
+	lcd_E_GPIO_Port->ODR |= (1<<lcd_E_bit);				// Set E to 1
+	LCD_ZeroPins();
+
+	 // Set to zero first
+	if(byte & 1<<7)	lcd_D7_GPIO_Port->ODR |= (1<<lcd_D7_bit);
+	if(byte & 1<<6)	lcd_D6_GPIO_Port->ODR |= (1<<lcd_D6_bit);
+	if(byte & 1<<5)	lcd_D5_GPIO_Port->ODR |= (1<<lcd_D5_bit);
+	if(byte & 1<<4)	lcd_D4_GPIO_Port->ODR |= (1<<lcd_D4_bit);
+
+	HAL_Delay(1);
+	lcd_E_GPIO_Port->ODR &= ~(1<<lcd_E_bit);		// Set E to 0
+}
+
+void LCD_ZeroPins()
+{
+	lcd_D7_GPIO_Port->ODR &= ~(1<<lcd_D7_bit);
+	lcd_D6_GPIO_Port->ODR &= ~(1<<lcd_D6_bit);
+	lcd_D5_GPIO_Port->ODR &= ~(1<<lcd_D5_bit);
+	lcd_D4_GPIO_Port->ODR &= ~(1<<lcd_D4_bit);
 }
 
 /* USER CODE END 4 */
